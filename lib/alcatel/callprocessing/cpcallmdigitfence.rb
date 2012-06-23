90b621be75287971fc6383bd15a65c686f05bced
R. Scott Shaffer <scott.shaffer@t-mobile.com>
2012-06-23 08:53:19 -0700

    initial implementation of cpcallmdigitfence

--- lib/alcatel/callprocessing/cpcallmdigitfence.rb
+++ lib/alcatel/callprocessing/cpcallmdigitfence.rb
@@ -1,20 +1,23 @@
 
-# require_relative '../resource_group'
+require_relative '../resource_group'
 
 module Alcatel
   module CallProcessing
     class CPCALLMDIGITFENCE
  
-      # include Alcatel::ResourceGroup
+      include Alcatel::ResourceGroup
  
       def self.dto
-        @dto ||= resource('DigitFence', fields) do
-          # include Resource::COUNTRYCODE
-        end
+        @dto ||= resource('DigitFence', fields)
       end
 
       def self.fields
-        %w[
+        %w[ DIGITFENCEINDEX TRANSLATIONGROUP ONEPLUSOPTION LEADINGDIGITSCUT LEADINGDIGITSADD EXPECTEDDIGITCOUNT
+            TERMINATIONTYPE SERVICEINDEX DIGITTYPE ROUTEACTIONTYPE ROUTEACTION OUTPULSE_01R_IND OUTPULSE_0ZZ_IND
+            CARRIERID MAXDIGITCOUNT LEADINGDIGITSADDTYPE ECHOCANCELLATION COUNTRYCODE OUTPUTNOA TARIFFGROUP
+            DESTINATIONTYPE CPC INTLPREFIXLEN NATTREESELECTOR STARTPOSITION SOURCENUMBER1 MODIFYDIGITINDEX1
+            SOURCENUMBER2 MODIFYDIGITINDEX2 SOURCENUMBER3 MODIFYDIGITINDEX3 SOURCENUMBER4 MODIFYDIGITINDEX4
+            SOURCENUMBER5 MODIFYDIGITINDEX5 TONEANNID TONEDURATION CALLGAPPFILTERID NOCHARGE
         ].map { |field_name| field_name.downcase.to_sym }
       end
 
--- test/callprocessing/tables/test_cpcallmdigitfence.rb
+++ test/callprocessing/tables/test_cpcallmdigitfence.rb
@@ -5,12 +5,19 @@ require_relative '../../../lib/alcatel/callprocessing/cpcallmdigitfence'
 class Test_CPCALLMDIGITFENCE < MiniTest::Unit::TestCase
 
   def self.fields
-    %w[
+    %w[ DIGITFENCEINDEX TRANSLATIONGROUP ONEPLUSOPTION LEADINGDIGITSCUT LEADINGDIGITSADD EXPECTEDDIGITCOUNT
+        TERMINATIONTYPE SERVICEINDEX DIGITTYPE ROUTEACTIONTYPE ROUTEACTION OUTPULSE_01R_IND OUTPULSE_0ZZ_IND
+        CARRIERID MAXDIGITCOUNT LEADINGDIGITSADDTYPE ECHOCANCELLATION COUNTRYCODE OUTPUTNOA TARIFFGROUP
+        DESTINATIONTYPE CPC INTLPREFIXLEN NATTREESELECTOR STARTPOSITION SOURCENUMBER1 MODIFYDIGITINDEX1
+        SOURCENUMBER2 MODIFYDIGITINDEX2 SOURCENUMBER3 MODIFYDIGITINDEX3 SOURCENUMBER4 MODIFYDIGITINDEX4
+        SOURCENUMBER5 MODIFYDIGITINDEX5 TONEANNID TONEDURATION CALLGAPPFILTERID NOCHARGE
     ].map { |field_name| field_name.downcase.to_sym }
   end
 
   def self.test_data
     [
+     [863,1,0,0,"                              ",11,1,1,4,8,1254,0,0,"0000",11,0,0,"      ",255,1,1,255,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+     [978,1,0,11,"5042619024                    ",11,1,1,4,8,238,0,0,"0000",11,0,0,"      ",255,1,1,255,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     ]
   end
 
@@ -22,14 +29,14 @@ class Test_CPCALLMDIGITFENCE < MiniTest::Unit::TestCase
     assert self.class.fields == @obj.class.fields
   end
 
-  def _test_enumerable
+  def test_enumerable
     assert_kind_of Enumerable, @obj
     assert @obj.entries.size == 2
     assert @obj.first
     assert @obj.respond_to? :each
   end
 
-  def _test_resource_has_expected_members
+  def test_resource_has_expected_members
     obj = @obj.first
     assert self.class.fields == obj.members
   end
