a77732289ee30b1fafe8831c12ee61c0e8c32dce
R. Scott Shaffer <scott.shaffer@t-mobile.com>
2012-06-24 10:20:34 -0700

    add test for dup digitfenceindex to test_cpcallmdigitfence

--- test/callprocessing/tables/test_cpcallmdigitfence.rb
+++ test/callprocessing/tables/test_cpcallmdigitfence.rb
@@ -18,6 +18,7 @@ class Test_CPCALLMDIGITFENCE < MiniTest::Unit::TestCase
     [
      [86,1,0,0,"                              ",3,1,101,16,2,63,0,0,"0000",3,0,0,"      ",255,1,1,255,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
      [225,1,0,0,"                              ",5,1,101,1,0,1,0,0,"0000",17,0,0,"      ",255,1,1,255,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
     ]
   end
 
@@ -60,6 +61,27 @@ class Test_CPCALLMDIGITFENCE < MiniTest::Unit::TestCase
 
     assert_raises RuntimeError do
       @obj.join epf
+    end
+  end
+
+  def test_join_with_same_digitfenceindex
+    epf_data = [
+      [201,"2                               ",1,1,2319,"2 1D-10D                        ",1],
+      [201,"2                               ",1,11,2319,"Restricted Dialing              ",1]
+    ]
+    df_data = [
+      [2319,1,0,0,"                              ",11,1,1,4,2,350,0,0,"    ",32,0,0,"      ",255,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [2319,1,0,0,"                              ",1,1,1,4,10,201,0,0,"    ",10,0,0,"      ",255,1,1,255,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    ]
+
+    epf = Alcatel::EmsData::EMSPREFIXFENCE.new(epf_data)
+    df = Alcatel::CallProcessing::CPCALLMDIGITFENCE.new(df_data)
+    epf = df.join epf
+
+    assert_block do
+      result = true
+      epf.each { |e| result = false unless e.expecteddigitcount == e.df.expecteddigitcount }
+      result
     end
   end
 
